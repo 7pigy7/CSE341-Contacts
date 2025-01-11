0ebeac9d00784cce11bd14bea778f78ffd839822
7pigy7 <7pigy7@gmail.com>
2025-01-11 12:43:13 -0800

    error corrections

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,7 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res) => {
     const result = await mongodb.getDatabase().db('project1').collection('contacts').find();
     result.toArray().then((contacts) => {
-        res.setHeader('Content - Type', 'application/json');
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);
     });
 };
@@ -13,7 +13,7 @@ const getSingle = async (req, res) => {
     const contactId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db('project1').collection('contacts').find({_id: contactId });
     result.toArray().then((contacts) => {
-        res.setHeader('Content - Type', 'application/json');
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
     });
 };
