199e66440e980908cd8d3cb973ddb916be0d3bd4
7pigy7 <7pigy7@gmail.com>
2025-01-31 19:34:10 -0800

    comparing

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,35 +3,35 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger-tags['Contacts']
-    const result = await mongodb.getDatabase().db('project1').collection('contacts').find();
-    result.toArray().then((contacts) => {
+    const result = await mongodb.getDatabase().db().collection('contacts').find();
+    result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts);
+        res.status(200).json(lists);
     });
 };
 
 const getSingle = async (req, res) => {
     //#swagger-tags['Contacts']
     const contactId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db('project1').collection('contacts').find({_id: contactId });
-    result.toArray().then((contacts) => {
+    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId });
+    result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);
+        res.status(200).json(lists[0]);
     });
 };
 
 const createContact = async (req, res) => {
     //#swagger-tags['Contacts']
     const contact = {
-        fistName: req.body.firstName,
+        firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday
     };
-    const response = await mongodb.getDatabase().db('project1').collection('contacts').add(contact);
+    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
     if (response.acknowledged) {
-        res.status(204).send();
+        res.status(201).json(response);
     } else {
         res.status(500).json(response.error || 'Some error occurred while creating the contact.');
     }
@@ -41,13 +41,14 @@ const updateContact = async (req, res) => {
     //#swagger-tags['Contacts']
     const contactId = new ObjectId(req.params.id);
     const contact = {
-        fistName: req.body.firstName,
+        firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday
     };
-    const response = await mongodb.getDatabase().db('project1').collection('contacts').modifyOne({_id: contactId}, contact);
+    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({_id: contactId}, contact);
+    console.log(response);
     if (response.modifiedCount > 0) {
         res.status(204).send();
     } else {
@@ -58,7 +59,7 @@ const updateContact = async (req, res) => {
 const deleteContact = async (req, res) => {
     //#swagger-tags['Contacts']
     const contactId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db('project1').collection('contacts').deleteOne({_id: contactId});
+    const response = await mongodb.getDatabase().db().collection('contacts').remove({_id: contactId}, true);
     if (response.deletedCount > 0) {
         res.status(204).send();
     } else {
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
-const app = express();
 
 const port = process.env.PORT || 3000;
+const app = express();
 
-app.use(bodyParser.json());
-app.use((req,res, next) => {
+app.use(bodyParser.json())
+    .use((req,res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    )
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     next();
 });
-app.use('/', require('./routes/index'));
+app.use('/', require('./routes'));
 
 
 mongodb.initDB((err) => {
